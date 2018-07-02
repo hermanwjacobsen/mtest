109c0a005cb767e4223de54e5381a211c7d25d8f
Herman W. Jacobsen <herman@wjacobsen.dk>
2018-07-02 14:00:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cpan -f install Tree::DAG_Node Test::Warn WWW::RobotRules HTTP::Cookies Net::FTP
 Text::Wrap Pod::Escapes Pod::Simple Pod::Man ExtUtils::MakeMaker HTTP::Server::Simple Time::Local HTTP::Date MIME::Base64 URI Encode Encode::Locale LWP::MediaTypes Compress::Raw::Bzip2 \
 Compress::Raw::Zlib IO::Uncompress::Inflate HTTP::Status ExtUtils::ParseXS Module::CoreList Module::Load Params::Check Module::Load::Conditional Locale::Maketext::Simple \
 IPC::Cmd ExtUtils::CBuilder Perl::OSType IO::Dir Version::Requirements Exporter CPAN::Meta::YAML JSON::PP Parse::CPAN::Meta CPAN::Meta Scalar::Util File::Spec \
-File::Temp version Module::Metadata Module::Build HTML::Tagset XSLoader HTML::Parser Sub::Uplevel Test::Exception HTML::TreeBuilder Test::Harness Test::More Pod::Usage WWW::Mechanize
+File::Temp version Module::Metadata Module::Build HTML::Tagset XSLoader HTML::Parser Sub::Uplevel Test::Exception HTML::TreeBuilder Test::Harness Test::More Pod::Usage WWW::Mechanize File::CountLines
 
 cpan -f install WWW::Mechanize::Timed
 
